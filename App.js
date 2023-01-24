21d78d1039af709e2a5610f767899dd2bc5ef322
Roosep <roopes16@hotmail.com>
2023-01-24 18:37:44 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const addition = () => {
+    let num = parseInt(number1) + parseInt(number2);
+    setResult(num);
+  }
+
+  const subtraction = () => {
+    let num = parseInt(number1) - parseInt(number2);
+    setResult(num)
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={{textAlign: "center"}}>Result: {result}</Text>
+        <TextInput style={{width:200, borderColor: 'gray', borderWidth:1}}  keyboardType = 'numeric' onChangeText={number1 => setNumber1(number1)}  value={number1}/>
+        <TextInput style={{width:200, borderColor: 'gray', borderWidth:1}}  keyboardType = 'numeric' onChangeText={number2 => setNumber2(number2)}  value={number2}/>
+      </View>
+      
+      <View style={{flexDirection: "row"}}>
+        <Button style={styles.button} title="+" onPress={addition} />
+        <Button style={styles.button} title='-' onPress={subtraction} />
+      </View>
     </View>
   );
 }
@@ -17,4 +40,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  button: {
+    marginTop: 8,
+    marginHorizontal: 10,
+    alignItems: "center"
+  },
+  });
